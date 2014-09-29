27cde753c572fc509a96768327b96eed81b216ee
weshays <weshays@gbdev.com>
2014-09-28 20:18:40 -0700

    Added tipsy library

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,19 +1,27 @@
-// Once the DOM is fully loaded then do this
-$(function() {
-  load_repos_for_weshays();
-});
+// // Once the DOM is fully loaded then do this
+// $(function() {
+// });
 
 
-function load_repos_for_weshays() {
-  $.get("https://api.github.com/users/weshays/repos", function(data) {
+function load_repos_for(username) {
+  $.get("https://api.github.com/users/"+username+"/repos", function(data) {
     $.each(data, function(indx, repo) {
       var last_updated_at = new Date( Date.parse(repo["updated_at"]) );
       var status = "active_repo";
       if(last_updated_at < Date.parse('2014-01-01') ) {
         status = "inactive_repo";
       }
-      $("#repos").append("<tr><td>weshays</td><td class='"+ status +"'>"+repo["full_name"]+"</td></tr>");
+      $("#repo_list").append("<tr><td>"+username+"</td><td class='" + status + "'>" +
+        "<a href='#' class='repo_descr' original-title='"+repo["description"]+"'>" + repo["full_name"] + "</a>" +
+       "</td></tr>");
     })
+    $(".repo_descr").tipsy({"gravity" : "w"});
   });
+}
+
+function load_data() {
+  $("#repo_list").html("");
+  var username = $("#username").val();
+  load_repos_for(username);
 }
 
